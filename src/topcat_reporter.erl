1d4f41e772c4f9f53048069cae21196fdb9ca4ca
Roger Lipscombe <roger@differentpla.net>
2014-03-10 18:49:58 +0000

    Tweak output.

--- src/topcat_reporter.erl
+++ src/topcat_reporter.erl
@@ -26,33 +26,33 @@ report_failed_tests(Tests) ->
     [report_failed_test(SuiteName, TestcaseName) || {SuiteName, TestcaseName} <- Tests].
 
 %report_ok_test(SuiteName, TestcaseName) ->
-%    io:format("\e[0;92m~p.~p...OK\e[0m~n", [SuiteName, TestcaseName]).
+%    io:format("\e[0;92m~p.~p: OK\e[0m~n", [SuiteName, TestcaseName]).
 
 report_skipped_test(SuiteName, TestcaseName) ->
-    io:format("\e[0;93m~p.~p...Skipped\e[0m~n", [SuiteName, TestcaseName]).
+    io:format("\e[0;93m~p.~p: Skipped\e[0m~n", [SuiteName, TestcaseName]).
 
 report_failed_test(SuiteName, TestcaseName) ->
-    io:format("\e[0;91m~p.~p...Failed\e[0m~n", [SuiteName, TestcaseName]).
+    io:format("\e[0;91m~p.~p: Failed\e[0m~n", [SuiteName, TestcaseName]).
 
 report_suite_starts(SuiteName) ->
-    io:format("\e[0;96m~p\e[0m~n", [SuiteName]).
+    io:format("\e[0;96m~p...\e[0m~n", [SuiteName]).
 
 report_testcase_starts(TestcaseName) ->
-    io:format("  \e[0;96m~p\e[0m~n", [TestcaseName]).
+    io:format("  \e[0;96m~p...\e[0m~n", [TestcaseName]).
 
 report_testcase_ends(TestcaseName, ok) ->
-    io:format("  \e[0;96m~p...\e[0;92m~s\e[0m~n", [TestcaseName, "OK"]);
+    io:format("  \e[0;96m~p: \e[0;92m~s\e[0m~n", [TestcaseName, "OK"]);
 report_testcase_ends(TestcaseName, skipped) ->
-    io:format("  \e[0;96m~p...\e[0;93m~s\e[0m~n", [TestcaseName, "Skipped"]);
+    io:format("  \e[0;96m~p: \e[0;93m~s\e[0m~n", [TestcaseName, "Skipped"]);
 report_testcase_ends(TestcaseName, {skipped, {failed, {_SuiteName, SetupName, {Reason, Stacktrace}}}}) ->
     % Skipped because init failed.
-    io:format("  \e[0;96m~p...\e[0;91m~s (~s Failed)\e[0m~n", [TestcaseName, "Skipped", SetupName]),
+    io:format("  \e[0;96m~p: \e[0;91m~s (~s Failed)\e[0m~n", [TestcaseName, "Skipped", SetupName]),
     io:format("  ~p~n", [Reason]),
     report_stacktrace(Stacktrace);
 report_testcase_ends(TestcaseName, failed) ->
-    io:format("  \e[0;96m~p...\e[0;91m~s\e[0m~n", [TestcaseName, "Failed"]);
+    io:format("  \e[0;96m~p: \e[0;91m~s\e[0m~n", [TestcaseName, "Failed"]);
 report_testcase_ends(TestcaseName, Status) ->
-    io:format("  \e[0;96m~p...\e[0;91m~p\e[0m~n", [TestcaseName, Status]).
+    io:format("  \e[0;96m~p: \e[0;91m~p\e[0m~n", [TestcaseName, Status]).
 
 report_stacktrace(Stacktrace) ->
     [report_stackframe(Frame) || Frame <- Stacktrace].
